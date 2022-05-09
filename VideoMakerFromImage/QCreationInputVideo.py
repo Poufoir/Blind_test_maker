aebc5ac34e5ab17c8f94774105a4d3b4c2c3fa39
Poufoir <clement.lemeunier44@gmail.com>
2022-05-09 14:24:19 +0200

    VideoMakingFromImage :     - Verify if paths exists

--- VideoMakerFromImage/QCreationInputVideo.py
+++ VideoMakerFromImage/QCreationInputVideo.py
@@ -1,5 +1,5 @@
 
-from PySide6.QtWidgets import  QWidget, QLineEdit, QGridLayout, QDialog, QFormLayout, QDoubleSpinBox, QPushButton
+from PySide6.QtWidgets import  QWidget, QLineEdit, QGridLayout, QDialog, QFormLayout, QDoubleSpinBox, QPushButton, QMessageBox
 from PySide6.QtCore import Qt
 from typing import Optional, Dict, Tuple, List, Callable
 import os
@@ -48,33 +48,36 @@ class QCreationInputVideo(QDialog):
         path_ffmpeg = self._path_ffmpeg_bin.text().replace("\\", "/")
         path_image = self._path_image.text().replace("\\", "/")
         path_video = self._path_video.text().replace("\\", "/")
-        if not(os.path.exists(path_ffmpeg) and os.path.exists(path_image)):
+        path_output = self._path_output.text().replace("\\", "/").replace('\u202a', '')
+        if not(os.path.exists(path_ffmpeg) and os.path.exists(path_image) and os.path.exists(path_video)):
+            QMessageBox.critical(self, "Problem with paths", "Some paths do not exists", QMessageBox.Ok, QMessageBox.Ok)
             return
-        actual_path = os.getcwd().replace("\\", "/")
-        # Size of video
-        subprocess_text = f'"{path_ffmpeg}/ffprobe" -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "{path_video}"'
-        return_size = subprocess.check_output(subprocess_text)
-        width, height = return_size.decode().split("\r")[0].split("x")
-        width, height = int(width), int(height)
+        try:
+            actual_path = os.getcwd().replace("\\", "/")
+            # Size of video
+            subprocess_text = f'"{path_ffmpeg}/ffprobe" -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "{path_video}"'
+            return_size = subprocess.check_output(subprocess_text)
+            width, height = return_size.decode().split("\r")[0].split("x")
+            width, height = int(width), int(height)
 
 
-        # Transform image with extension of video
-        extension_video = "." +path_video.split(".")[-1]
-        directory_image, image = os.path.split(path_image)
-        image_without_extension = image.split(".")[0]
-        subprocess_image_text = f'"{path_ffmpeg}/ffmpeg" -loop 1 -i "{path_image}" -c:v libx264 -t {self._timer_image.value()} -pix_fmt yuv420p -vf scale={width}:{height} "{actual_path}/image{extension_video}"'
-        subprocess.call(subprocess_image_text)
+            # Transform image with extension of video
+            extension_video = "." +path_video.split(".")[-1]
+            subprocess_image_text = f'"{path_ffmpeg}/ffmpeg" -loop 1 -i "{path_image}" -c:v libx264 -t {self._timer_image.value()} -pix_fmt yuv420p -vf scale={width}:{height} "{actual_path}/image{extension_video}"'
+            subprocess.call(subprocess_image_text)
 
-        # Transform video
-        extension_video = "." +path_video.split(".")[-1]
-        directory_video, video = os.path.split(path_video)
-        image_without_extension = image.split(".")[0]
-        subprocess_video_text = f'"{path_ffmpeg}/ffmpeg" -i "{path_video}" -c copy -t {self._timer_video.value()} "{actual_path}/video{extension_video}"'
-        subprocess.call(subprocess_video_text)
+            # Transform video
+            extension_video = "." +path_video.split(".")[-1]
+            subprocess_video_text = f'"{path_ffmpeg}/ffmpeg" -i "{path_video}" -c copy -t {self._timer_video.value()} "{actual_path}/video{extension_video}"'
+            subprocess.call(subprocess_video_text)
 
-        # Merge video and image
-        path_output = self._path_output.text().replace("\\", "/")
-        subprocess_merge = f'"{path_ffmpeg}/ffmpeg" -i "{actual_path}/video{extension_video}" -i "{actual_path}/image{extension_video}" -filter_complex "[0:v][1:v] concat=n=2:v=1:a=0 [outv] " -vsync 2 -map "[outv]" "{path_output}"'
-        subprocess.call(subprocess_merge)
-        os.remove(f'{actual_path}/video{extension_video}')
-        os.remove(f'{actual_path}/image{extension_video}')
+            # Merge video and image
+            
+            subprocess_merge = f'"{path_ffmpeg}/ffmpeg" -i "{actual_path}/video{extension_video}" -i "{actual_path}/image{extension_video}" -filter_complex "[0:v][1:v] concat=n=2:v=1:a=0 [outv] " -vsync 2 -map "[outv]" "{path_output}"'
+            subprocess.call(subprocess_merge)
+            os.remove(f'{actual_path}/video{extension_video}')
+            os.remove(f'{actual_path}/image{extension_video}')
+            QMessageBox.information(self, "Sucess", "Video Created", QMessageBox.Ok, QMessageBox.Ok)
+            self.accept()
+        except Exception as e:
+            QMessageBox.critical(self, "Convertion problem", f"{type(e)} - {str(e)}", QMessageBox.Ok, QMessageBox.Ok)
